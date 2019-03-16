9c28784c36415769ceb41bd37c081e3f89c9f60a
subbuplatina <subbuplatina@gmail.com>
2019-03-16 08:20:44 +0530

    Maven Project.

---
+++ MavenFirstProject/src/main/java/HCL/MavenFirstProject/App.java
@@ -0,0 +1,15 @@
+package HCL.MavenFirstProject;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+    public void sample() {
+    	System.out.println("This is sample Maven & TestNG method");
+    }
+}
